96e0dc2da28d8dbb93eeba36432398475743a0c8
Abel Bekele <95418375+switch247@users.noreply.github.com>
2023-08-14 10:08:23 +0300

    description

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
 -- show full description 
-SELECT CREATING FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME='first_table';
+SELECT description FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME='first_table';
 -- DESC TABLE 'first_table'
