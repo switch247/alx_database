2bf6434d3dd390bf44bc83129dd6bbbd2317694a
Abel Bekele <95418375+switch247@users.noreply.github.com>
2023-08-14 10:09:35 +0300

    desc

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
 -- show full description 
-SELECT COLUMNS FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME='first_table';
--- DESC TABLE 'first_table'
+-- SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME='first_table';
+DESC TABLE 'first_table'
