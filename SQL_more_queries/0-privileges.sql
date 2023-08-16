1744acb87231402b87392a496d3762ee4b19f745
Abel Bekele <95418375+switch247@users.noreply.github.com>
2023-08-16 14:35:07 +0300

    grant 0

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,7 +1,4 @@
 -- afd
-SELECT
-    user, host, grant_priv
-FROM
-    mysql.user
-WHERE
-    user IN ('user_0d_1', 'user_0d_2');
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
+
