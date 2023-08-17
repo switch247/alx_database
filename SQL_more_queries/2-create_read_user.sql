3e37a88f69736f27ed22747fd39ca16e901c0317
Abel Bekele <95418375+switch247@users.noreply.github.com>
2023-08-17 09:45:38 +0300

    t2

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,1 +1,9 @@
--- 
+-- Create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
+
+-- Create the user if it doesn't exist
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Grant SELECT privilege to the user on the database
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
+-- database(d_2).table(*)
