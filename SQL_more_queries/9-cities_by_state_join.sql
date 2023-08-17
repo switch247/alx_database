4dfecdd262809e6298e3eff613fcddbe5dd41f6b
Abel Bekele <95418375+switch247@users.noreply.github.com>
2023-08-17 10:41:53 +0300

    T9

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,7 +1,7 @@
 --  test
 SELECT  cities.id, cities.name, states.name
 FROM cities
-OUTER JOIN states ON cities.state_id = states.id
+JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
 
 
