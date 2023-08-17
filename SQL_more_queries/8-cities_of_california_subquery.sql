d0a06d417b47fc4cb387f4ab6b9cc2a64eba8a80
Abel Bekele <95418375+switch247@users.noreply.github.com>
2023-08-17 10:23:29 +0300

    T8

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,7 @@
 --  DISPLAY
 
-SELECT * FROM cities, states
+SELECT cities.id, cities.name
+FROM cities, states
 WHERE cities.state_id = states.id
 AND states.name = 'California'
 ORDER BY cities.id ASC;
