7e66d3a2761be19150690372d8148238da3c55dc
Abel Bekele <95418375+switch247@users.noreply.github.com>
2023-08-17 10:41:26 +0300

    T9

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,8 @@
 --  test
-SELECT dISTINCT (cities.id, cities.name, states.name)
-FROM cities, states
+SELECT  cities.id, cities.name, states.name
+FROM cities
+OUTER JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
+
+
+
